9a04d79cd15fb5e4a8cadc016382bb042bb09ca5
Bryan Jinez <119009737+21edclique@users.noreply.github.com>
2024-05-20 13:13:17 -0500

    Add files via upload

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      // Agrega aquí los archivos que contienen las pruebas del módulo 'login'
+      // Por ejemplo, si tus pruebas del login están en 'src/app/login/login.component.spec.ts'
+      // deberías agregar 'src/app/login/*.spec.ts'
+      'src/app/modules/auth/components/login/login.component.spec.ts'
+    ],
+    exclude: [],
+    preprocessors: {
+      // Agrega aquí cualquier procesamiento adicional si es necesario
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
