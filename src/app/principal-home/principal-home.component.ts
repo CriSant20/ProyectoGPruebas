47e51bd4b98933d5169ece058d47c0caeefda97e
CriSant20 <csantamaria8643@uta.edu.ec>
2024-05-22 12:31:59 -0500

    Pagina home del club

---
+++ src/app/principal-home/principal-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './principal-home.component.html',
+  styleUrl: './principal-home.component.css'
+})
+export class PrincipalHomeComponent {
+
+}
